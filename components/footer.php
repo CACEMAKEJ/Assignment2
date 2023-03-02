76518e42bb87bf52ab86314d691f048572537c39
Manin Macina <Feedesson12@gmail.com>
2023-03-02 00:01:03 +0000

    added footer file

---
+++ components/footer.php
@@ -0,0 +1,12 @@
+<div class="b-example-divider"></div>
+
+<footer class="d-flex justify-content-between align-items-center border-top pt-2 mt-4">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
+      </a>
+      <span class="mb-3 mb-md-0 py-2 my-2 text-muted">&copy; <?php echo date("Y") ?>  Manoncin, Inc</span>
+    </div>
+
+  </footer>
+    
